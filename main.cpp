c4c62f0ed853e1a222a5534a131d3a91ad7a63ba
yweyer <yweyer@btslivh.eu>
2018-12-18 14:51:15 +0100

    commit main test1

---
+++ main.cpp
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: snir2g2
+ *
+ * Created on 18 décembre 2018, 14:49
+ */
+
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    std::cout<<"test"<<std::endl;
+    return 0;
+}
+
